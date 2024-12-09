52f9f77b718f82ec5d13c30b5c1df838bdd2e856
Maksymilian Cych <maksymilian@cych.eu>
2024-12-09 10:52:13 +0100

    Update shell.nix

--- shell.nix
+++ shell.nix
@@ -1,9 +1,17 @@
-{pkgs ? import <nixpkgs> {}}:
-pkgs.mkShell {
-  nativeBuildInputs = with pkgs; [rustc cargo gcc rustfmt clippy];
+let
+  # Last updated: 2/26/21. Update as necessary from https://status.nixos.org/...
+  # pkgs = import (fetchTarball "https://github.com/NixOS/nixpkgs/archive/04ac9dcd311956d1756d77f4baf9258392ee7bdd.tar.gz") {};
+  # Rolling updates, not deterministic.
+  pkgs = import (fetchTarball "channel:nixpkgs-unstable") {};
+in
+  pkgs.mkShell {
+    buildInputs = with pkgs; [
+      cargo
+      rustc
+      rustfmt
+      pkg-config
+    ];
 
-  # Certain Rust tools won't work without this
-  # This can also be fixed by using oxalica/rust-overlay and specifying the rust-src extension
-  # See https://discourse.nixos.org/t/rust-src-not-found-and-other-misadventures-of-developing-rust-on-nixos/11570/3?u=samuela. for more details.
-  RUST_SRC_PATH = "${pkgs.rust.packages.stable.rustPlatform.rustLibSrc}";
-}
+    # See https://discourse.nixos.org/t/rust-src-not-found-and-other-misadventures-of-developing-rust-on-nixos/11570/3?u=samuela.
+    RUST_SRC_PATH = "${pkgs.rust.packages.stable.rustPlatform.rustLibSrc}";
+  }
