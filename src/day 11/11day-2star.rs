2aaa304cf1bc28b25bd46079fa496f2f4e98a9e4
Cyliann <maksymilian@cych.eu>
2024-12-11 17:58:28 +0100

    Day 11 - Star 2

---
+++ src/day 11/11day-2star.rs
@@ -0,0 +1,105 @@
+use std::{collections::HashMap, fs::read_to_string};
+
+fn main() {
+    let stones = read_input("./src/day 11/input.txt");
+
+    let map = blink(stones, 75);
+    let mut sum: u64 = 0;
+    for val in map.values() {
+        sum = sum + val;
+    }
+    println!("{:?}", sum);
+}
+
+fn blink(stones: Vec<u64>, num_of_blinks: u64) -> HashMap<u64, u64> {
+    let mut memo = HashMap::new();
+    let mut to_insert;
+    let mut to_remove;
+
+    for stone in stones.iter() {
+        if let Some(val) = memo.get_mut(stone) {
+            *val += 1 as u64;
+        } else {
+            memo.insert(*stone, 1 as u64);
+        }
+    }
+
+    for j in 0..num_of_blinks {
+        to_insert = vec![];
+        to_remove = vec![];
+        for (key, value) in memo.iter() {
+            if *key == 0 {
+                to_insert.push((1, *value));
+                to_remove.push(*key);
+                continue;
+            }
+            let length = get_length(*key);
+            if length % 2 == 0 {
+                let mask = (10 as u64).pow(length / 2);
+                let left = *key / mask;
+                let right = *key % mask;
+
+                to_insert.push((left, *value));
+                to_insert.push((right, *value));
+                to_remove.push(*key);
+
+                continue;
+            }
+
+            to_insert.push((*key * 2024, *value));
+            to_remove.push(*key);
+        }
+
+        for key in to_remove.iter() {
+            memo.remove(key);
+        }
+        for (key, value) in to_insert {
+            if let Some(val) = memo.get_mut(&key) {
+                *val += value;
+                continue;
+            }
+            memo.insert(key, value);
+        }
+        println!("Blink: {}", j + 1);
+    }
+    return memo;
+}
+
+// faster than parsing to string
+fn get_length(mut number: u64) -> u32 {
+    let mut length = 1;
+    if number >= 10000000000000000 {
+        length += 16;
+        number /= 10000000000000000;
+    }
+    if number >= 100000000 {
+        length += 8;
+        number /= 100000000;
+    }
+    if number >= 10000 {
+        length += 4;
+        number /= 10000;
+    }
+    if number >= 100 {
+        length += 2;
+        number /= 100;
+    }
+    if number >= 10 {
+        length += 1;
+    }
+    return length;
+}
+
+fn read_input(path: &str) -> Vec<u64> {
+    let mut stones = vec![];
+
+    for line in read_to_string(path).unwrap().lines() {
+        stones.extend(
+            line.split(" ")
+                .map(|x| x.parse::<u64>().unwrap())
+                .collect::<Vec<u64>>(),
+        );
+    }
+
+    return stones;
+}
